395a2c4b07835e4fcd23b3f3d96963385b6d7559
Vamshi-27 <vamshira18@outlook.com>
2024-12-02 20:57:20 +0530

    sync

---
+++ ui.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import yaml
+from build_vocab import Vocabulary
+from inference import Predictor
+
+# Load model configuration
+with open("config.yaml", "rb") as rb:
+    config = yaml.load(rb, yaml.FullLoader)
+    
+model = Predictor(config)
+
+# UI Components
+st.title("Image Captioning Model")
+
+st.write("Upload your image and Generate Caption automatically")
+
+image = st.file_uploader("Insert Image")
+
+
+if st.button("Generate Caption"):
+    if image is None:
+        st.write("Insert image to generate Caption")
+        
+    else:
+        prediction = model.predict(image)
+        prediction = prediction.strip(".").strip()
+        st.write(prediction)
+
+# End
